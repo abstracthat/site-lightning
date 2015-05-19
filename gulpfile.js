799ba8f23d4ef94eb1ac042b4afde981fd59a8b7
Josh Bertrand <josh.bertrand@gmail.com>
2015-05-19 14:07:23 -0400

    remove server from local gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,6 @@ var gulp = require("gulp");
 var lightning = require("gulp-lightning");
 
 var config = {};
-config.server = "projects:/var/www/sitelightning.co";
+config.server = "";
 
 lightning(gulp, config);
